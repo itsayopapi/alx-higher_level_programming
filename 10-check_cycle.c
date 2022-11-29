e8bcfc308df59d57a4f0deab961abdfbbac2adff
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2022-11-29 13:06:00 +0200

    Python tasks 1-9

---
+++ 10-check_cycle.c
@@ -0,0 +1,29 @@
+
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list - pointer to the list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	tortoise = list;
+	hare = list;
+
+	while (tortoise && hare)
+	{
+		if (hare->next == NULL)
+			return (0);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
+	}
+
+	return (0);
+}
