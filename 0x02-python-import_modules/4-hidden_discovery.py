d982cf73bf6a9ab17f77938628c2846578bcf927
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2022-12-01 17:03:51 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
