c13439fb36480d158aee1cee96473c693d71fa11
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2022-12-05 05:23:56 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    """This method returns a 'new_string' through mapping of the old strings charcters"""
-    new_string = my_string.translate({ord(letter): None for letter in 'cC'})
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
